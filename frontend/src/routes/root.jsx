be311b1caf1f047376042509b8329fc76c1575da
dbchau10 <82007790+dbchau10@users.noreply.github.com>
2023-11-13 15:09:25 +0700

    update route

--- frontend/src/routes/root.jsx
+++ frontend/src/routes/root.jsx
@@ -5,20 +5,28 @@ import CoursesSection from "../components/CoursesSection";
 import BenefitSection from "../components/BenefitSection";
 import GetStarted from "../components/GetStarted";
 import Footer from "../components/Footer";
-import MiniDrawer from "../components/Drawer";
 export default function Root() {
   return (
     <Box
     sx={{
-     display: "flex",
-     position: "absolute",
-     top: 0,
-     right: 0,
-     left: 0,
-     minHeight: "100vh"
+      backgroundImage: "url(../src/assets/landing-background.png)",
+      position: "absolute",
+      top: 0,
+      right: 0,
+      left: 0,
+      minHeight: "100vh",
+      backgroundSize: "100%",
+      backgroundRepeat: "no-repeat",
     }}
   >
-    <MiniDrawer/>
+    <Container>
+      <IntroSection />
+      <ImpactSection />
+      <CoursesSection />
+      <BenefitSection />
+      <GetStarted />
+    </Container>
+    <Footer />
   </Box>
   );
 }
