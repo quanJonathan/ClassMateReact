8ad6b33de5ca95cd89784a458cb4ba3f8d0c382c
dbchau10 <82007790+dbchau10@users.noreply.github.com>
2023-11-13 17:57:06 +0700

    route fix

--- frontend/App.jsx
+++ frontend/App.jsx
@@ -15,15 +15,14 @@ import { ProtectedLayout } from "./src/components/ProtectedLayout";
 import ProfilePage from "./src/routes/profile";
 import { useAuth } from "./src/hook/useAuth";
 
-
 const AppRoutes = () => {
-  const  isAuthenticated  = useAuth();
+
 
   return (
     <Routes>
       <Route element={<AuthLayout />}>
         <Route element={<HomeLayout />}>
-          <Route path="/" element={isAuthenticated ? <Dashboard/> : <Root />} />
+          <Route path="/" element={ useAuth ? <Dashboard/> : <Root />} />
           <Route path="/sign-in" element={<SignIn />} />
           <Route path="/sign-up" element={<SignUp />} />
         </Route>
